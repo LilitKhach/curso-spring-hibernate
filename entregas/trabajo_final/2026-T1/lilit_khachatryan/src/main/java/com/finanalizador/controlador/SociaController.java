021001e750b9a5d2a61cc4381057506a7050c4e1
Lilit <lilitkhachatryan189@gmail.com>
2026-03-19 10:22:23 +0100

    Add summaries for endpoints

--- entregas/trabajo_final/2026-T1/lilit_khachatryan/src/main/java/com/finanalizador/controlador/SociaController.java
+++ entregas/trabajo_final/2026-T1/lilit_khachatryan/src/main/java/com/finanalizador/controlador/SociaController.java
@@ -26,6 +26,7 @@ public class SociaController {
     }
 
     @PostMapping
+    @Operation(summary = "Registrat una socia")
     public ResponseEntity<Socia> registrarSocia(@Valid @RequestBody Socia socia) {
         try {
             var sociaRegistrada = sociaService.registrarSocia(socia);
@@ -38,12 +39,14 @@ public class SociaController {
     }
 
     @GetMapping
+    @Operation(summary = "Obtener todos socias")
     public ResponseEntity<List<Socia>> buscarTodosSocias() {
         var socias = sociaService.buscarSocias();
         return ResponseEntity.ok(socias);  // 200 OK
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener una socia por ID")
     public ResponseEntity<Socia> buscarSociaPorId(@PathVariable Long id){
         Socia socia = sociaService.buscarSociaPorId(id);
         return ResponseEntity.ok(socia);
@@ -51,6 +54,7 @@ public class SociaController {
 
     // Get beneficios for a specific Socia from-to date
     @GetMapping("{id}/beneficio")
+    @Operation(summary = "Obtener un beneficio de la socia")
     public ResponseEntity<BigDecimal> buscarBeneficioDeSocia(
             @PathVariable Long id,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
@@ -60,6 +64,7 @@ public class SociaController {
     }
 
     @GetMapping("/dashboard")
+    @Operation(summary = "Obtener socia beneficio para dashboard")
     public ResponseEntity<List<SociaBeneficioDTO>> obtenerDashboard(
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
@@ -70,6 +75,7 @@ public class SociaController {
     }
 
     @GetMapping("/dashboard/summary")
+    @Operation(summary = "Obtener datos para dashboard")
     public ResponseEntity<DashboardSummaryDTO> obtenerResumenDashboard(
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
@@ -80,12 +86,14 @@ public class SociaController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar la socia")
     public ResponseEntity<Socia> actualizarSocia(@PathVariable Long id, @Valid @RequestBody Socia socia){
         var sociaActualizada = sociaService.actualizarSocia(id, socia);
         return ResponseEntity.ok(sociaActualizada); // Status = 200
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar la socia")
     public ResponseEntity eliminarSocia(@PathVariable Long id){
         sociaService.eliminarSocia(id);
         return ResponseEntity.noContent().build(); // Status = 204
